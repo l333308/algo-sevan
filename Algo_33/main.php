f087b42d98d03600b637ad428d2f57ddbf49c3ba
SevAn_ <1010922659@qq.com>
2021-03-10 18:42:40 +0800

    Add StringMatchAlgo class.

---
+++ Algo_33/main.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Algo_33;
+require_once "../vendor/autoload.php";
+
+class StringMatchAlgo{
+
+    public $sourceString;
+
+    public $targetString;
+
+    public function __construct($sourceString, $targetString)
+    {
+
+    }
+}
